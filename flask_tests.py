32caeedf2e0cbace6c749336515c83334df7cc87
Michal <mkowalik@outlook.com>
2016-04-21 23:38:46 +0200

    use different stylesheets

---
+++ flask_tests.py
@@ -0,0 +1,20 @@
+import os
+import app
+import unittest
+import tempfile
+
+
+class FlaskTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
+
+
+if __name__ == "__main__":
+    unittest.main()
